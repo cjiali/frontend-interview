cebc0d76debf09e71e78e5f1e1f664371a56f297
cjiali <chjiali@outlook.com>
2020-11-08 03:46:52 +0800

    feat: add mock promise implement

---
+++ test/promise.spec.js
@@ -0,0 +1,26 @@
+const { $Promise } = require("../src/promise");
+
+describe("#promise", () => {
+    test("executor resolved", (done) => {
+        let promise = new $Promise((resolve, reject) => {
+            setTimeout(function () {
+                resolve(true);
+                //reject(true)
+            }, 100);
+        });
+
+        promise.then(
+            (value) => {
+                try {
+                    expect(value).toBe(true);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            },
+            (reason) => {
+                // console.log('onRejected:', reason);
+            },
+        );
+    });
+});
